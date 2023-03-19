fbc243e97788e78c642f5aa5114c270015852b42
achmadrivaldi <mrtampan54@gmail.com>
2023-03-20 00:47:10 +0700

    first commit

---
+++ public/build/assets/Home-9722383c.js
@@ -0,0 +1,1 @@
+import{o as n,c as s}from"./app-adf1364f.js";const l=(e,o)=>{const t=e.__vccOpts||e;for(const[c,r]of o)t[c]=r;return t},_={};function a(e,o){return n(),s("h1",null,"Hello World")}const m=l(_,[["render",a]]);export{m as default};
